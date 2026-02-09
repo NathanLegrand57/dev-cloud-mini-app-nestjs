102e22e711e2a6b74be1754ad8c263b04be83c49
NathanLegrand57 <nathanlgd14@gmail.com>
2026-02-09 14:23:39 +0100

    Rename getHello to getError in AppController to align with service method

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,8 +6,8 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getError(): string {
+    return this.appService.getError();
   }
 
   @Get('health')
