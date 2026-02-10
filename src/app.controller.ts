987bee09d83185101c664ea2da9081e2109d5cb4
NathanLegrand57 <nathanlgd14@gmail.com>
2026-02-10 15:29:07 +0100

    Implement FoodService with CRUD operations and update AppController for food management

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,17 +1,36 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Put, Body, Param } from '@nestjs/common';
 import { AppService } from './app.service';
+import { FoodService } from './Services/foodService.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private foodService: FoodService,
+  ) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
   }
 
-  @Get('health')
+  @Get('api/health')
   getHealth(): object {
     return { status: 'ok' };
+  }
+
+  @Get('api/food')
+  getFoodList() {
+    return this.foodService.getFoodList();
+  }
+
+  @Post('api/food')
+  addFood(@Body() food: any) {
+    return this.foodService.addFood(food);
+  }
+
+  @Put('api/food/:id')
+  updateFood(@Param('id') id: string, @Body() updatedFood: any) {
+    return this.foodService.updateFood(+id, updatedFood);
   }
 }
